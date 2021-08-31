dd2d5d25c6b67348920fca903ef5fc578b2d7f76
johnnysamuael <61773527+johnnysamuael@users.noreply.github.com>
2021-08-31 20:42:06 +0530

    Update and rename flamassignment.php to index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <title>Flam Assignment :</title>
 	<h1> Flam Backend Assignment</h1>
-	<h2> This form sends a post request with the following parameters (Image, quality, height and width). If height and width are not specified the quality is reduced as per the parameter inputted. If quality anf the hxw are provided, the image is compressed and resized.</h2>
+	<h2> This form sends a post request with the following parameters (Image, quality, height and width). If height and width are not specified the quality is reduced as per the parameter inputted. If quality and the HxW are provided, the image is compressed and resized.</h2>
 	<h3> This can be scaled up with CDN integrated with apache also by load balancing. As per the time constraint, the images are resized and compressed as per request. This can be later converted to a API based service. This also works as an API if post a image along with the parameters. You can use postman or Hoppscotch to test it out (parameters: image_file,quality,height,width) with form encoded header. </h3>
 	<h3> This creates a token based on the timestamp and appends it with every image uploaded so we there is no clash with other service requests with the same name. We also can use the temporary image path provided from the local device but its vulnerable to change with refresh or no activity. I have also included a satement at the end of compression which shows if its original size vs compressed size</h3>
 	<h4> Certain test cases such as the quality cannot be less than 0 or higher than 100, or the file not being uploaded and other basic test cases have been checked and tested</h4>
@@ -96,10 +96,10 @@ if ($_SERVER['REQUEST_METHOD'] == "POST")
     <form action='' method='POST' enctype='multipart/form-data'>
         <input name="image_file" type="file" accept="image/*">
 		<label for="blan">:</label><br>
-		<label for="fname">Quality:</label><br>
+		<label for="fname">Quality%</label><br>
 		<input name="quality" type="number">
-		<label for="blan">:</label><br>
-		<label for="fname">Height:</label><br>
+		<label for="blan">%</label><br>
+		<label for="fname">Height</label><br>
 		<input name="height" type="number">
 		<label for="blan">:</label><br>
 		<label for="fname">WIdth:</label><br>
